86f9e2c8fc1137db64e2d28cc1f2a6ca6169dd80
1633944 <1633944@uab.cat>
2024-11-13 19:50:35 +0100

    Partida Inicial

---
+++ src/main/model/Partida.java
@@ -0,0 +1,14 @@
+package main.model;
+
+public class Partida {
+    private Jugador jugador1;
+    private Jugador jugador2;
+
+    public Jugador getJugador1() {
+        return jugador1;
+    }
+
+    public Jugador getJugador2() {
+        return jugador2;
+    }
+}
