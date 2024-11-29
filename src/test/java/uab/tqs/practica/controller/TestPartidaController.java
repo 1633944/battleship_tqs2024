bf855ae66956331a429930c272b00ee1e45ebd58
1633944 <92081716+1633944@users.noreply.github.com>
2024-11-29 11:02:08 +0100

    Actualització tests

--- src/test/java/uab/tqs/practica/controller/TestPartidaController.java
+++ src/test/java/uab/tqs/practica/controller/TestPartidaController.java
@@ -39,7 +39,62 @@ public class TestPartidaController {
         // Instanciar el controlador
         controller = new PartidaController(partida, vista);
     }
+/*
+    @Test
+    public void testIniciarPartida() {
+        // Simular que los jugadores colocan los barcos
+        doNothing().when(jugador1).colocarBarcos();
+        doNothing().when(jugador2).colocarBarcos();
 
+        // Ejecutar el inicio de la partida
+        controller.iniciarPartida();
+
+        // Verificar que ambos jugadores han colocado sus barcos
+        verify(jugador1, times(1)).colocarBarcos();
+        verify(jugador2, times(1)).colocarBarcos();
+
+        // Verificar que se haya mostrado el tablero para ambos jugadores
+        verify(vista, times(2)).mostrarTablero(any());
+    }
+
+    @Test
+    public void testTurnoJugador1() {
+        // Simulación de disparo
+        when(jugador1.recibirDisparo(0, 0)).thenReturn("Tocado");
+        when(jugador2.recibirDisparo(0, 0)).thenReturn("Agua");
+
+        // Simular un disparo exitoso
+        boolean resultado = controller.disparar(jugador1, jugador2);
+
+        // Verificar que se haya disparado correctamente y el resultado sea el esperado
+        verify(vista).mostrarMensaje("Resultado del disparo: Tocado");
+        assertTrue(resultado);
+    }
+
+    @Test
+    public void testTurnoJugador2() {
+        // Simulación de disparo
+        when(jugador2.recibirDisparo(1, 1)).thenReturn("Agua");
+
+        // Simular un disparo fallido
+        boolean resultado = controller.disparar(jugador2, jugador1);
+
+        // Verificar que se haya disparado correctamente y el resultado sea el esperado
+        verify(vista).mostrarMensaje("Resultado del disparo: Agua");
+        assertFalse(resultado);
+    }
+
+    @Test
+    public void testFinDeJuego() {
+        // Simulamos que jugador2 pierde
+        when(jugador1.haPerdido()).thenReturn(true);
+
+        // Iniciar el juego y verificar que el mensaje de fin de juego se muestra
+        controller.iniciarPartida();
+
+        verify(vista).mostrarMensaje("¡GANADOR JUGADOR 1!");
+    }
+*/
     @Test
     public void testLimpiarConsola() {
         // Capturar la salida de la consola
